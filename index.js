ef85d2e3db5a4cdf9508efef36d4bd0d88ebdb43
GGIEnrike <enrike.churin@gmail.com>
2022-07-04 19:30:20 +0200

    1 file modified, 2 deleted

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
 const express = require('express');
 const app = express();
-const port = 3010;
+const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
+const port = 3010;
 
-app.use(express.static('static'));
+const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
+const r_path = __dirname + '/views/';
+app.use(express.static(r_path));
+
+app.get('/video/:id', (req, res) => {
+  const path = `assets/${req.params.id}/${req.params.id}.mp4`;
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+    if (start >= fileSize) {
+      res
+        .status(416)
+        .send('Requested range not satisfiable\n' + start + ' >= ' + fileSize);
+      return;
+    }
+
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(path, { start, end });
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
 });
 
+app.get('/captions/:id', (req, res) => {
+  res.sendFile(__dirname + `/assets/${req.params.id}/${req.params.id}.vtt`);
+});
+
+app.get('/list', (req, res) => {
+  res.sendFile(__dirname + '/assets/assets.json');
+});
+
+app.use(cors());
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  require('dns').lookup(require('os').host  name(), function (err, add, fam) {
+    console.log('Listening on ' + add + ':4000');
+  });
 });
